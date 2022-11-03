baa4d6ceabfabe4e04f99cf03658f648dca54154
ridlwanyunus <hiddenjustice309@gmail.com>
2022-11-03 08:22:31 +0700

    Modified: Adding description

--- RidlwanYunus.pas
+++ RidlwanYunus.pas
@@ -1,19 +1,10 @@
-{
-
-                            Online Pascal Compiler.
-                Code, Compile, Run and Debug Pascal program online.
-Write your code in this editor and press "Run" button to execute it.
-
-}
-
-
 program RidlwanYunus;
 
 Uses crt, sysutils;
 
 Type
-    pItem = ^tItem;
-    tItem = record
+    pItem = ^tItem;     { Pointer yang menyimpan alamat pada record tItem }
+    tItem = record      { Record yang memuat attribute attribute pada nota }
                 id : integer;
                 namaMenu : string[30];
                 jumlah : integer;
@@ -27,20 +18,23 @@ var
     newValue : string[30];
     pilihan : string[1];
 
+{ Fungsi untuk menambah node pada list }
 function add(last : pItem; content : tItem): pItem;
 var 
     cur : pItem;
 begin
-    new(cur);
-    cur^.id:=content.id;
-    cur^.namaMenu:=content.namaMenu;
+    new(cur);       { Mendefinisikan variabel disertai dengan alamat baru }
+    cur^.id:=content.id;     { Insialisasi pointer dengan value }
+    cur^.namaMenu:=content.namaMenu; { pada temporary record content }
     cur^.jumlah:=content.jumlah;
     cur^.jumlahHarga:=content.jumlahHarga;
     cur^.next:=nil; 
+    
     if last<>nil then last^.next:=cur; 
     add:=cur; 
 end;
 
+{ Prosedur untuk mengedit salah satu attribute pada node }
 procedure update(var head : pItem; updateNode : pItem; index: integer; newValue: string);
 var cur : pItem;
 begin
@@ -50,18 +44,19 @@ begin
     cur:=updateNode; 
     
     if index=1 then
-        cur^.id := strToInt(newValue);
+        cur^.id := strToInt(newValue);  { mengubah nilai pada attribute id }
      
     if index=2 then
-        cur^.namaMenu := newValue;
+        cur^.namaMenu := newValue;      { mengubah nilai pada attribute namaMenu }
      
     if index=3 then
-        cur^.jumlah := strToInt(newValue);
+        cur^.jumlah := strToInt(newValue); { mengubah nilai pada attribute jumlah }
     
     if index=4 then
-        cur^.jumlahHarga := strToInt(newValue);
+        cur^.jumlahHarga := strToInt(newValue); { mengubah nilai pada attribute jumlahHarga }
 end;
 
+{ Prosedur untuk menghapus node berdasarkan node yang ditemukan }
 procedure delete(var head : pItem; var tail: pItem; searchedItem : pItem);
 var 
     bef, cur, aft : pItem;
@@ -69,52 +64,53 @@ begin
     if head=nil then 
         exit;
     
-    cur:=searchedItem; 
-    aft:=cur^.next;
-    bef:=head;
+    cur:=searchedItem;  { pointer current = node yang dicari }
+    aft:=cur^.next;     { pointer after = current + 1 di depan }
+    bef:=head;          { pointer before = pointer head (first node) }
     
-    if bef<>cur then
+    if bef<>cur then    { jika head = node yang dicari }
     begin
-        while (bef^.next<>cur) and (bef<>nil) do 
+        while (bef^.next<>cur) and (bef<>nil) do { loop mulai dari head sampai pointer ditemukan }
         begin   
             
-            bef:=bef^.next;
-            if (bef^.next= tail) and (cur = tail) then
-            begin
-                tail := bef;
+            bef:=bef^.next; { node = node + 1 }
+            if (bef^.next= tail) and (cur = tail) then { cek apakah node yang dicari adalah tail (last node) }
+            begin   
+                tail := bef;    { tail = node sebelum node yang dicari }
             end;
         end;
-        bef^.next:=aft;
-        dispose(cur);
+        bef^.next:=aft;         { 1 node sebelum node yang dicari disambungkan node yang dicari + 1  }
+        dispose(cur);           { Hapus node yang dicari  }
     end
-    else
+    else                { jika head != node yang dicari }
     begin
-        dispose(head); 
-        head:=aft;
+        dispose(head);  { hapus head }
+        head:=aft;      { head = head + 1 node }
     end;
 end;
 
+{ Fungsi untuk mencari node berdasarkan id }
 function find(head : pItem; id: integer):pItem;
 var 
     cur : pItem; 
     found : boolean;
 begin
-    cur:=head;
+    cur:=head;              { current node = head }
     while cur<>nil do
     begin
-        if cur^.id=id then
+        if cur^.id=id then  { cek apakah attribute id pada current node sama dengan id yang dicari }
         begin
             found:=true; break;
         end;
-        cur:=cur^.next;
+        cur:=cur^.next;     { current node = node + 1 }
     end;
 
     if found then 
     begin
-        find:=cur;
+        find:=cur;          { return current node jika ditemukan }
     end
     else 
-        find:=nil;
+        find:=nil;          { return null node jika tidak }
 end;
 
 function printFormat(word: string; limit: integer): string;
@@ -123,11 +119,11 @@ var
     wordFormatted: string;
 begin
     
-    diff := limit - Length(word);
+    diff := limit - Length(word);   { selisih panjang limit - panjang input kata }
     wordFormatted := word;
     for i:=1 to diff do
     begin
-        wordFormatted := wordFormatted + ' ';
+        wordFormatted := wordFormatted + ' '; { isi selisih dengan spasi ' ' }
     end;
     printFormat := wordFormatted;
 end;
@@ -168,15 +164,15 @@ begin
     writeln(printFormat('(v): Lihat',15),printFormat('(a): Tambah',15),printFormat('(e): Edit',15),printFormat('(d): Hapus', 15), printFormat('(x): Keluar', 15));
     write('Pilihan: '); readln(pilihan);
 
-    if pilihan = 'x' then
+    if pilihan = 'x' then       { Exit }
     begin
         break;
     end
-    else if pilihan = 'v' then
+    else if pilihan = 'v' then  { View }
     begin
         display(head);
     end
-    else if pilihan = 'a' then
+    else if pilihan = 'a' then  { Add }
     begin
         write(printFormat('id',15),': '); readln(temp.id);
         write(printFormat('Nama',15),': '); readln(temp.namaMenu);
@@ -186,7 +182,7 @@ begin
         if head=nil then head:=last;
         display(head);
     end
-    else if pilihan = 'e' then
+    else if pilihan = 'e' then  { Edit }
     begin
         write('Edit Node: '); readln(searchedIndex);
         searchedItem := find(head, searchedIndex);
@@ -203,7 +199,7 @@ begin
         update(head, searchedItem, searchedField, newValue);
         display(head);
     end
-    else if pilihan = 'd' then
+    else if pilihan = 'd' then  { Delete }
     begin
         write('Delete Node: '); readln(searchedIndex);
         searchedItem := find(head, searchedIndex);
